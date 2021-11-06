8bdf570c274642bf8c690c95f79c275018845ab8
jinwoo <jintakk6531@gmail.com>
2021-11-06 16:50:16 +0900

    11/6

--- app.ts
+++ app.ts
@@ -320,7 +320,6 @@ const newsFeedView = new NewsFeedView('root');
 const newsDetailView = new NewsDetailView('root');
 
 router.setDefaultPage(newsFeedView);
-
 router.addRoutePath('/page/', newsFeedView);
 router.addRoutePath('/show/', newsDetailView);
 router.route();
